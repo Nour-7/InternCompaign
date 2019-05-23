99ed372c5a3cabba3fabbca435741cac6ebf271c
Nour7 <nour7011@gmail.com>
2019-05-23 22:07:27 +0200

    Get test case

--- test/app.js
+++ test/app.js
@@ -9,106 +9,18 @@ describe('GET /api/compaigns', () => {
   });
 });
 
-describe('GET /login', () => {
+describe('GET /api/compaigns/n1', () => {
   it('should return 200 OK', (done) => {
     request(app)
-      .get('/login')
+      .get('/api/compaigns/n1')
       .expect(200, done);
   });
 });
 
-describe('GET /signup', () => {
-  it('should return 200 OK', (done) => {
+describe('GET /api/compaigns/n6', () => {
+  it('should return 404 Not Found', (done) => {
     request(app)
-      .get('/signup')
-      .expect(200, done);
-  });
-});
-
-describe('GET /api', () => {
-  it('should return 200 OK', (done) => {
-    request(app)
-      .get('/api')
-      .expect(200, done);
-  });
-});
-
-describe('GET /contact', () => {
-  it('should return 200 OK', (done) => {
-    request(app)
-      .get('/contact')
-      .expect(200, done);
-  });
-});
-
-describe('GET /api/lastfm', () => {
-  it('should return 200 OK', (done) => {
-    request(app)
-      .get('/api/lastfm')
-      .expect(200, done);
-  });
-});
-
-describe('GET /api/twilio', () => {
-  it('should return 200 OK', (done) => {
-    request(app)
-      .get('/api/twilio')
-      .expect(200, done);
-  });
-});
-
-describe('GET /api/stripe', () => {
-  it('should return 200 OK', (done) => {
-    request(app)
-      .get('/api/stripe')
-      .expect(200, done);
-  });
-});
-
-describe('GET /api/scraping', () => {
-  it('should return 200 OK', (done) => {
-    request(app)
-      .get('/api/scraping')
-      .expect(200, done);
-  });
-});
-
-describe('GET /api/lob', () => {
-  it('should return 200 OK', (done) => {
-    request(app)
-      .get('/api/lob')
-      .expect(200, done);
-  });
-});
-
-describe('GET /api/aviary', () => {
-  it('should return 200 OK', (done) => {
-    request(app)
-      .get('/api/aviary')
-      .expect(200, done);
-  });
-});
-
-describe('GET /api/clockwork', () => {
-  it('should return 200 OK', (done) => {
-    request(app)
-      .get('/api/clockwork')
-      .expect(200, done);
-  });
-});
-
-describe('GET /api/upload', () => {
-  it('should return 200 OK', (done) => {
-    request(app)
-      .get('/api/upload')
-      .expect(200, done);
-  });
-});
-
-describe('GET /random-url', () => {
-  it('should return 404', (done) => {
-    request(app)
-      .get('/reset')
+      .get('/api/compaigns/n6')
       .expect(404, done);
   });
 });
